ef17bf4fcfbdc2d9a501c7a17f9a16ab5b973674
EduardoRFS <theeduardorfs@gmail.com>
2019-08-24 21:02:01 -0300

    shouldn't support const, just let

--- example.js
+++ example.js
@@ -1,8 +1,7 @@
-// let sub = (a, b) => a - b;
-// eslint-disable-next-line
+/* eslint-disable prefer-const */
 let not = a => 0 - a;
-const mul = (a, b) => {
-  const internal = (acc, a, b) =>
+let mul = (a, b) => {
+  let internal = (acc, a, b) =>
     b == 1 ? acc : internal(acc - not(a), a, b - 1);
   internal(a, a, b);
 };
